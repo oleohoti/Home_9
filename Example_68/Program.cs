22e5fcb04ee6f04c4f1e4dade3acf08cffd2a250
Vladimir <vova.okhotin@mail.ru>
2022-12-25 13:39:42 +0800

    68

--- Example_68/Program.cs
+++ Example_68/Program.cs
@@ -1,19 +1,33 @@
 ﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
-int Akkerman(int m, int n)
+// int Akkerman(int m, int n)
+// {
+//     int a =0;
+//     if (m==0) a=n+1;
+//     else if (m>0 && n==0) a = Akkerman(m-1,n);
+//     else if(m>0 && n>0) a=Akkerman(m-1,Akkerman(m,n-1));
+//     return a ;
+//     // else if (m>0 && n>0) return Akkerman(m-1,Akkerman(m,n-1));
+// }
+static uint Akkerman(uint n, uint m)
 {
-    if (m==0) return n+1;
-    else if (m>0 && n==0) return 2^m-3;
-    // else if (m>0 && n>0) return Akkerman(m-1,Akkerman(m,n-1));
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Akkerman(n - 1, 1);
+    else
+      return Akkerman(n - 1, Akkerman(n, m - 1));
 }
 try 
 {
 Console.WriteLine("Введите неотрицательное целое число m");
-int m = Convert.ToInt32(Console.ReadLine());
+uint m = Convert.ToUInt32(Console.ReadLine());
 Console.WriteLine("Введите неотрицательное целое число n");
-int n = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine(Akkerman(m,n));
+uint n = Convert.ToUInt32(Console.ReadLine());
+uint ak=Akkerman(m,n);
+Console.WriteLine(ak);
 }
 catch
 {
